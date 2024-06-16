d65d7bd872011f220d1658e788213b7a09b799c7
huda044 <hudaacil00@gmail.com>
2024-06-16 12:22:46 +0700

    huda:membuat tampilan login

--- Program/app/src/main/java/com/uti/indonesea/MainActivity.kt
+++ Program/app/src/main/java/com/uti/indonesea/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.uti.indonesea
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,29 @@ import androidx.fragment.app.Fragment
 import com.uti.indonesea.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var usernameInpute : EditText
+    lateinit var passwordInpute : EditText
+    lateinit var loginBtn : Button
+
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(HomeFragment())
+
+        usernameInpute = findViewById(R.id.username_input)
+        passwordInpute = findViewById(R.id.password_input)
+        loginBtn = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener {
+            val username = usernameInpute.text.toString()
+            val password = passwordInpute.text.toString()
+            Log.i("Test Credentials", "Username : $username and Password $password")
+
+        }
+
 
         binding.btnMenu.setOnItemSelectedListener {
             when(it.itemId){
