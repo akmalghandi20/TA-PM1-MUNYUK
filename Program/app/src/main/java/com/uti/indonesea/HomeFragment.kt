f8747fdcc6730d0286a08e82d3809844f707efe1
akmalghandi20 <gandolkikok@gmail.com>
2024-06-16 19:58:41 +0700

    master : menghubungkan imgPenyu ke Artikel1Fragment

--- Program/app/src/main/java/com/uti/indonesea/HomeFragment.kt
+++ Program/app/src/main/java/com/uti/indonesea/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.uti.indonesea.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +35,11 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.imgPenyu.setOnClickListener{
+            requireActivity().supportFragmentManager.beginTransaction().addToBackStack(null).replace(R.id.frm_lay,Artikel1Fragment()).commit()
+        }
+        return binding.root
     }
 
     companion object {
